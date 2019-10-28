acfb079ded2887a7753fa4aab26ece38b64d30f8
Eddy G <eddienyagano@gmail.com>
2019-10-28 10:30:57 +0200

    Added Album and tracks

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,18 +9,18 @@ import {Product} from './product';
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
-  private _productUrl = '../assets/products.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) {
   }
 
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map((response) => <Album>response.json())
+    return this._http.get(this._albumUrl).map(response => <Album>response.json())
 
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productUrl).map((response) => <Product[]>response.json())
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json())
 
   }
 }
