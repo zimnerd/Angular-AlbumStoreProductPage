3e5583453db4a6ccc0dc845bc5ad6ee28cd66b01
Eddy G <eddienyagano@gmail.com>
2019-10-28 09:35:33 +0200

    Added description

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -12,7 +12,7 @@ export class ProductDescriptionComponent implements OnInit {
   }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe((response) => this.albumInfo = response)
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
 }
