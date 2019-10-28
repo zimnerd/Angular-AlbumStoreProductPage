366ca3cf3eee9db6be6346247bf4ca61dbd1f419
Eddy G <eddienyagano@gmail.com>
2019-10-28 09:26:53 +0200

    Added description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ProductPageComponent } from './product-page/product-page.component';
-import { ProductDescriptionComponent } from './product-description/product-description.component';
+import {AppComponent} from './app.component';
+import {ProductPageComponent} from './product-page/product-page.component';
+import {ProductDescriptionComponent} from './product-description/product-description.component';
+import {ProductService} from './product.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ProductDescriptionComponent } from './product-description/product-descr
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
